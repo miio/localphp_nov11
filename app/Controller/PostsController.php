c895ceda721a538dff9ec4a863462539cb24726a
miio mitani <miio@ubuntu.(none)>
2011-11-25 22:32:05 -0800

    add edits

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -20,4 +20,17 @@ class PostsController extends AppController{
             }
         }
     }
+
+    public function edit($id = null){
+        $this->Post->id = $id;
+        if($this->request->is('get')){
+            $this->request->data = $this->Post->read();
+           // $this->set('post',$this->Post->read());
+        }else{
+            if($this->Post->save($this->request->data)){
+                $this->Session->setFlash('Your post has been updated.');
+                $this->redirect(array('action'=>'index'));
+            }
+        }
+    }
 }
