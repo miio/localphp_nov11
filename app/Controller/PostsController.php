00ff500f4307402ae9b32818c1d25b10d09385d0
miio mitani <miio@ubuntu.(none)>
2011-11-25 22:35:32 -0800

    add delete

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -33,4 +33,14 @@ class PostsController extends AppController{
             }
         }
     }
+
+    public function delete($id){
+        if(!$this->request->is('post')){
+            throw new MethodNotAllowedExeption();
+        }
+        if($this->Post->delete($id)){
+            $this->Session->setFlash('The post with id: '.$id.' has been deleted.');
+            $this->redirect(array('action'=>'index'));
+        }
+    }
 }
